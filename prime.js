c05a6582e1961e93b9bc990afd6bf3efe5114f9d
snark-coder <happy.durga2458@gmail.com>
2024-03-15 08:51:50 +0530

    website

---
+++ prime.js
@@ -0,0 +1,21 @@
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("awards");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1){slideIndex=slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+       
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); 
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlides(slideIndex);
+}
